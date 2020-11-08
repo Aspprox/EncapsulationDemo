765e414b5b2c70b06efc69d34846c4627ea2c467
ozan.yesil <ozan.yesil@ANK-YZLORT2-09>
2020-11-08 19:29:35 +0300

    Add project files.

---
+++ EncapsulationDemo/Customer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulationDemo
+{
+    class Customer
+    {
+        // fields (alanlar)
+        string _adSoyad;
+        public string AdSoyad
+        {
+            get
+            {
+                if (ErkekMi)
+                    return "Bay " + _adSoyad;
+                // else olmasa daha iyi olur return bulduğu an çıkıyor zaten.metoddan çıkıyor.
+                return "Bayan " + _adSoyad;
+            }
+            set
+            {
+                _adSoyad = value; //value senin set ettiğin değer demek
+            }
+        }
+
+        //public void SetAdSoyad(string AdSoyad) //property kullanmak daha mantıklı
+        //{
+        //    _AdSoyad = AdSoyad;
+        //}
+
+        //public string GetAdSoyad()
+        //{
+        //    return _AdSoyad;
+        //}
+        public bool ErkekMi { get; set; } //property  //yukarıdaki metod oluşturma ile aynı işlev
+
+
+
+
+
+        string _kartNo;  //özelleştirmeden dolayı
+
+        public string KartNo
+        {
+            //1234 5678 9012 3456
+            get
+            {
+                return "**** **** **** " + _kartNo.Substring(15);
+            }
+            set
+            {
+                _kartNo = value;
+            }
+        }
+    }
+}
---
+++ EncapsulationDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulationDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+            customer.AdSoyad = "Çağıl Alsaç";
+            customer.ErkekMi = true;
+            customer.KartNo = "1234 5678 9012 3456";
+            Console.WriteLine("Adı: " + customer.AdSoyad + "\n" + "Kart No: " + customer.KartNo);
+            Console.ReadLine();
+        }
+    }
+}
